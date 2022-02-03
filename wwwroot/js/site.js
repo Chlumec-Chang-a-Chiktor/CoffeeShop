adca57e326376995eee6a02f0960e099e2030db2
Antonín Višňák <antoninvisnak01@zak.spsul.cz>
2022-02-03 14:53:30 +0100

    Revert "Revert "page transitions whoa""
    This reverts commit 0e021168e39e07ae7e9ae2f0d26e4f1831592183.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const options = {
+    cache: true
+};
+const swup = new Swup(options);
